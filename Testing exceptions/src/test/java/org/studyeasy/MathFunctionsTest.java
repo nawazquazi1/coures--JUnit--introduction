86464577c71319d459fa70be5c2b0986d2c244d8
nawaz quazi <nawazquazi356@gmail.com>
2023-05-20 16:55:18 +0530

    JUnit 5

---
+++ Testing exceptions/src/test/java/org/studyeasy/MathFunctionsTest.java
@@ -0,0 +1,43 @@
+package org.studyeasy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathFunctionsTest {
+
+	@Test
+	void testAdd() {
+		MathFunctions mathFunctions = new MathFunctions();
+		int expected = 40;
+		int actual = mathFunctions.add(10, 30);
+		assertEquals(expected, actual, "Addition operation failed");
+	}
+
+	@Test
+	void testSub() {
+		MathFunctions mathFunctions = new MathFunctions();
+		int expected = -20;
+		int actual = mathFunctions.sub(10, 30);
+		assertEquals(expected, actual, "Substraction operation failed");
+	}
+
+	@Test
+	void testMultiple() {
+		MathFunctions mathFunctions = new MathFunctions();
+		int expected = 300;
+		int actual = mathFunctions.multiple(10, 30);
+		assertEquals(expected, actual, "Muiltipication operation failed");
+	}
+
+	@Test
+	void testDivide() {
+		MathFunctions mathFunctions = new MathFunctions();
+		int expected = 0;
+		int actual = mathFunctions.divide(10, 30);
+		assertEquals(expected, actual, () -> "Division operation failed");
+		assertThrows(ArithmeticException.class, () -> mathFunctions.divide(10, 0),
+				() -> "ArithmeticException expected");
+
+	}
+}
